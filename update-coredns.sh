a4166c81761b59760c82c90f4c2f764b1d16c30b
Anton Zyablov <anton0zyablov@gmail.com>
2026-03-17 23:42:46 +0100

    feat: check for kubectl existence

--- update-coredns.sh
+++ update-coredns.sh
@@ -7,18 +7,23 @@ INGRESS_SVC="${INGRESS_SVC:-nok-apps-ingress}"
 SCRIPT_DIR="${SCRIPT_DIR:-/core-dns-editor}"
 CORE_DNS_CONFIG="${CORE_DNS_CONFIG:-/tmp/coredns.yaml}"
 
-cd "$SCRIPT_DIR"
+cd "$SCRIPT_DIR" # full path to script directory
 
-echo "Installing kubectl ${KUBECTL_VERSION}"
-curl -L -o ./kubectl https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl
-chmod +x ${SCRIPT_DIR}/kubectl
+KUBECTL_BIN="${SCRIPT_DIR}/kubectl"
+if [ -x "$KUBECTL_BIN" ]; then
+  echo "kubectl already exists at $KUBECTL_BIN, skipping download"
+else
+  echo "Installing kubectl ${KUBECTL_VERSION}"
+  curl -L -o ./kubectl https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl
+  chmod +x "$KUBECTL_BIN"
+fi
 
 # install python dependencies
 pip install -r "${SCRIPT_DIR}/requirements.txt"
 
 # get external IP or hostname of ingress controller
-IP=$(${SCRIPT_DIR}/kubectl -n $INGRESS_NS get ingress $INGRESS_SVC -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-FQDN=$(${SCRIPT_DIR}/kubectl -n $INGRESS_NS get ingress $INGRESS_SVC -o jsonpath='{.spec.rules[0].host}')
+IP=$($KUBECTL_BIN -n $INGRESS_NS get ingress $INGRESS_SVC -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+FQDN=$($KUBECTL_BIN -n $INGRESS_NS get ingress $INGRESS_SVC -o jsonpath='{.spec.rules[0].host}')
 
 if [ -z "$IP" ] && [ -z "$FQDN" ]; then
   echo "ERROR: ingress controller has no external IP/hostname"
@@ -29,7 +34,7 @@ TARGET="$IP"
 if [ -z "$TARGET" ]; then TARGET="$FQDN"; fi
 
 # get Corefile, patch in hosts entry
-${SCRIPT_DIR}/kubectl -n kube-system get cm coredns -o yaml > ${CORE_DNS_CONFIG}
+$KUBECTL_BIN -n kube-system get cm coredns -o yaml > ${CORE_DNS_CONFIG}
 python3 ${SCRIPT_DIR}/coredns_editor.py ${CORE_DNS_CONFIG} --ip $IP --hostname $FQDN -i
-${SCRIPT_DIR}/kubectl -n kube-system replace cm coredns -f  ${CORE_DNS_CONFIG}
-${SCRIPT_DIR}/kubectl -n kube-system rollout restart deployment/coredns
+$KUBECTL_BIN -n kube-system replace cm coredns -f  ${CORE_DNS_CONFIG}
+$KUBECTL_BIN -n kube-system rollout restart deployment/coredns
